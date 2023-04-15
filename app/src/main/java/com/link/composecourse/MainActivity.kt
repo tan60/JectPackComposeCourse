14f2097a28a96fbfc8c451be49e410c01fe61e8b
dhkim <dohyoung81@gmail.com>
2023-04-15 09:13:02 +0900

    Row, Column, Basing Sizing

--- app/src/main/java/com/link/composecourse/MainActivity.kt
+++ app/src/main/java/com/link/composecourse/MainActivity.kt
@@ -3,13 +3,22 @@ package com.link.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.link.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,34 +26,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Greeting(name = "Mustapa")
+            Row(modifier = Modifier
+                //.fillMaxSize(0.5f)
+                .width(200.dp)
+                .height(400.dp)
+                .background(Color.Green),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround) {
+                Text(text = "hello")
+                Text(text = "hello")
+                Text(text = "hello")
+                //Alignment, Arrengment가 다르구나
+                //Modifier가 일종의 Container 느낌, 배치되는 영역 및 배경 색 등을 정할 수 있음
+                //fillMaxSize > fill_parent와 같은 옵션, 기본은 warp_parent, width, height 등을 설정할 수 있음
+            }
         }
 
 
-        /*setContent {
-            ComposeCourseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }*/
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Greeting("haha notworking")
-}
