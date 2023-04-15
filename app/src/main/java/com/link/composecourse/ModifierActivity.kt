e4e5743b0c96daadaf7e054e49ed738b1c2dd909
dhkim <dohyoung81@gmail.com>
2023-04-15 10:21:39 +0900

    card composable course

--- app/src/main/java/com/link/composecourse/ModifierActivity.kt
+++ app/src/main/java/com/link/composecourse/ModifierActivity.kt
@@ -6,15 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
 
 class ModifierActivity : ComponentActivity() {
@@ -41,8 +49,10 @@ class ModifierActivity : ComponentActivity() {
                 Text("World")
             }
         }
-
-
-
     }
 }
+
+
+
+
+
