a18027aa9415b3b1ce3b91a69ea100ff6adb72c6
dhkim <dohyoung81@gmail.com>
2023-04-15 14:00:05 +0900

    Lists Course

---
+++ app/src/main/java/com/link/composecourse/ListsActivity.kt
@@ -0,0 +1,43 @@
+package com.link.composecourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class ListsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            //LazyColumn을 사용하고 itemIdexed에 data item을 list 형태로 set
+            //하단에 adapter 마냥 UI와 연결 해주면 리스트 UI 쉽게 그릴 수 있다.
+            LazyColumn {
+                itemsIndexed(
+                    listOf("This", "is", "Jetpeck", "Compose")
+                ) { index, string ->
+                    Text(
+                        text = string,
+                        textAlign = TextAlign.Center,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+
+                    )
+                }
+            }
+        }
+    }
+}
