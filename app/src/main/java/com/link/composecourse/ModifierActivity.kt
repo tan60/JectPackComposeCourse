4019f63a9f0299eb11f7f82d45c549c51c85e344
dhkim <dohyoung81@gmail.com>
2023-04-15 09:35:33 +0900

    Modifier course

---
+++ app/src/main/java/com/link/composecourse/ModifierActivity.kt
@@ -0,0 +1,48 @@
+package com.link.composecourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class ModifierActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Column(modifier = Modifier
+                .background(Color.Green)
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth()
+                .border(5.dp, Color.Magenta)
+                .padding(5.dp)
+                .border(5.dp, Color.Blue)
+                .padding(5.dp)
+                .border(10.dp, Color.Red)
+                .padding(10.dp)
+                //.requiredWidth(600.dp) //min width > 영역이 화면을 벋어날 수 있다
+            ) {
+                Text("Hello", modifier = Modifier.clickable {
+
+                })
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("World")
+            }
+        }
+
+
+
+    }
+}
